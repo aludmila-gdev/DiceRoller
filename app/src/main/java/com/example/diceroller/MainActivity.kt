c4d28e8b0de3b71d9d1a2fbe6c6efad71a6f23d8
Ana Ludmila <kn.ludi@gmail.com>
2022-03-26 10:31:53 -0300

    @knludi - Add images to the Dice Roller app

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
+        rollDice()
 
         rollButton.setOnClickListener {
             rollDice()
@@ -20,18 +21,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultDice1: TextView = findViewById(R.id.textViewResultDice1)
-        val resultDice2: TextView = findViewById(R.id.textViewResultDice2)
-        val dice = Dice(6)
-        resultDice1.text = dice.roll().toString()
-        resultDice2.text = dice.roll().toString()
+        val diceImageView : ImageView = findViewById(R.id.diceImageView)
+        val roll = Dice(6).roll()
+
+        diceImageView.contentDescription = roll.toString()
+
+        when(roll){
+            1 ->  diceImageView.setImageResource(R.drawable.dice_1)
+            2 ->  diceImageView.setImageResource(R.drawable.dice_2)
+            3 ->  diceImageView.setImageResource(R.drawable.dice_3)
+            4 ->  diceImageView.setImageResource(R.drawable.dice_4)
+            5 ->  diceImageView.setImageResource(R.drawable.dice_5)
+            6 ->  diceImageView.setImageResource(R.drawable.dice_6)
+        }
     }
 }
 
 class Dice(private val numSides: Int) {
 
     fun roll(): Int {
-
         return (1..numSides).random()
     }
 }
